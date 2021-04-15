8c8157c0851cabc9948a5bae01b81d9a4f41d0fb
vamsigutha <vamsigutha27@gmail.com>
2021-04-15 22:02:06 +0530

    completed pagination in product category, started working on product detail page

--- src/app/features/productcategory/productcategory.component.ts
+++ src/app/features/productcategory/productcategory.component.ts
@@ -25,16 +25,36 @@ export class ProductcategoryComponent implements OnInit {
     this.route.params.subscribe((params:Params)=>{
       this.category = params.category;
       this.subCategory = params.subCategory;
-      this.productCategoryService.getProductsByCategory(params.category,params.subCategory).subscribe(result=>{
-        this.products = result["products"];
-        this.totalItems = result["totalItems"];
-        this.currentPageItems = result["currentPageItems"];
-        this.totalPages = result["totalPages"];
-        this.currentPage = result["currentPage"];
-        
-      },
-      error=>console.log("Error occured"));
+      this.performRequest();
     });
+  }
+
+  onPrevPage(){
+    if(this.currentPage != 1){
+      this.productCategoryService.page = `${this.currentPage-1}`;
+      this.performRequest();
+      window.scrollTo(0,0);
+    }
+  }
+
+  onNextPage(){
+    if(this.currentPage < this.totalPages ){
+      this.productCategoryService.page = `${this.currentPage+1}`;
+      this.performRequest();
+      window.scrollTo(0,0);
+    }
+  }
+
+  performRequest(){
+    this.productCategoryService.getProductsByCategory(this.category,this.subCategory).subscribe(result=>{
+      this.products = result["products"];
+      this.totalItems = result["totalItems"];
+      this.currentPageItems = result["currentPageItems"];
+      this.totalPages = result["totalPages"];
+      this.currentPage = result["currentPage"];
+      
+    },
+    error=>console.log("Error occured"));
   }
 
 
--- src/app/features/productcategory/services/product-category.service.ts
+++ src/app/features/productcategory/services/product-category.service.ts
@@ -10,12 +10,13 @@ export class ProductCategoryService {
 
   
   url:string = "http://localhost:8765/api/v1/products/category";
+  page:string="1";
 
 constructor(private http:HttpClient) { }
 
 getProductsByCategory(category,subCategory){
   
-  let params = new HttpParams().set("category",category).set("subCategory",subCategory);
+  let params = new HttpParams().set("category",category).set("subCategory",subCategory).set("page",this.page);
 
   return this.http.get(this.url,{params:params});
 }
